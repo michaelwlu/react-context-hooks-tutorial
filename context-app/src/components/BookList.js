863169298507c07fa45992a82ab2f1ddf40fc72c
michaelwlu <michael.lu@stern.nyu.edu>
2020-06-10 22:45:54 -0400

    Complete tutorial

---
+++ context-app/src/components/BookList.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
+
+/* Class Component with Context
+
+class BookList extends Component {
+  static contextType = ThemeContext;
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <div
+        className="book-list"
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
+        <ul>
+          <li style={{ background: theme.ui }}>The Way of Kings</li>
+          <li style={{ background: theme.ui }}>The Name of the Wind</li>
+          <li style={{ background: theme.ui }}>The Final Empire</li>
+        </ul>
+      </div>
+    );
+  }
+}
+*/
+
+// Functional Component with Context
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
+  const theme = isLightTheme ? light : dark;
+
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        {books.map((book) => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>
+              {book.title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
---
+++ context-app/src/components/Navbar.js
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
+
+/* Class Component Consuming Multiple Contexts
+
+class Navbar extends Component {
+  render() {
+    return (
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                  <h1>Context App</h1>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? 'Logged in' : 'Logged out'}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
+    );
+  }
+}
+
+*/
+
+// Functional Component Consuming Multiple Contexts
+const Navbar = () => {
+  // Consume contexts and destructure
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+
+  const theme = isLightTheme ? light : dark;
+
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div onClick={toggleAuth}>
+        {isAuthenticated ? 'Logged in' : 'Logged out'}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
